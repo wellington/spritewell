4db69b57dd79398ab49bb87209dc5e33994b0315
Drew Wells <drew.wells00@gmail.com>
2014-12-28 23:35:59 -0600

    Inline uses readers and automatically detects png, jpg, gif, and svg

--- inline.go
+++ inline.go
@@ -35,9 +35,9 @@ func IsSVG(r io.Reader) bool {
 	return false
 }
 
-// Inline accepts an io.Reader and returns a byte array of
-// base64 encoded binary data or optionally base64 encodes
-// svg data.
+// Inline accepts an io.Reader and writes to io.Writer.  Binary
+// data is base64 encoded, but base64 encoding is optional for
+// svg.
 func Inline(r io.Reader, w io.Writer, encode ...bool) error {
 
 	// Check if SVG
@@ -59,28 +59,6 @@ func Inline(r io.Reader, w io.Writer, encode ...bool) error {
 	return err
 }
 
-// InlineSVG accepts a byte slice and returns a valid utf8 svg+xml bytes.
-// Any invalid utf8 runes are removed, unnecessary newline and whitespace
-// are removed from the input.  This encoding is more error prone, but uses
-// less space.
-func InlineSVG(r io.Reader, w io.Writer) {
-	w.Write([]byte(`url("data:image/svg+xml;utf8,`))
-	io.Copy(w, r)
-	w.Write([]byte(`")`))
-}
-
-// InlineSVG accepts a byte slice and returns a base64 byte slice
-// compatible with image/svg+xml;base64
-func InlineSVGBase64(in []byte) []byte {
-	/*enc := inlineSVG(in, true)
-	out := make([]byte, 0, len(enc)+40)
-	out = []byte(`url("data:image/svg+xml;base64,`)
-	out = append(out, enc...)
-	out = append(out, []byte(`")"`)...)
-	return out*/
-	return []byte{}
-}
-
 // inlinesvg returns a byte slice that is utf8 compliant or base64
 // encoded
 func inlineSVG(w io.Writer, r io.Reader, encode bool) {
@@ -94,19 +72,18 @@ func inlineSVG(w io.Writer, r io.Reader, encode bool) {
 
 	w.Write([]byte(`url("data:image/svg+xml;utf8,`))
 	// Exhaust the buffer and do some sloppy regex stuff to the input
-	// TODO: convert this to streaming reader
+	// TODO: convert url encoding to streaming reader/writer
 	var buf bytes.Buffer
 	buf.ReadFrom(r)
 
-	// // Strip unnecessary whitespace and newlines
+	// Strip unnecessary newlines
 	input := bytes.Replace(buf.Bytes(), []byte("\r\n"), []byte(""), -1)
 	// input = bytes.Replace(input, []byte(`"`), []byte("'"), -1)
 	// reg := regexp.MustCompile(`>\\s+<`)
 	// input = reg.ReplaceAll(input, []byte("><"))
 
+	// url.String() properly escapes paths
 	u := &url.URL{Path: string(input)}
-	encodedPath := []byte(u.String())
-
-	w.Write(encodedPath)
+	io.WriteString(w, u.String())
 	w.Write([]byte(`")`))
 }
